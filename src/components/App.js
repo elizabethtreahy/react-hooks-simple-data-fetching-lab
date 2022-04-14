a1cc22ad624eb0ecb9859f29dc5e51fc4b0d3551
Elizabeth <elizabethtreahy@gmail.com>
2022-04-13 20:25:08 -0400

    commit tree 2888e3aa9eca396150c40e7494a28723c751a251

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,18 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((r) => r.json())
+      .then(x => setData(x.message))
+  }, [])
+
+  if (!data) return <p>Loading...</p>;
+
+  return <img src={data} alt="A Random Dog" />;
+}
+export default App
+
